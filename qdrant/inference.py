0d519f36f297e01167610618c18302d40e2a495e
kimmypracha <pracha.promtaow@gmail.com>
2025-11-23 16:55:44 -0800

    update the llm output and llm as a judge evaluation

--- qdrant/inference.py
+++ qdrant/inference.py
@@ -1,6 +1,7 @@
 # run this file to make inference requests: python qdrant/inference.py
 # modify QUERY and COLLECTION_NAME as needed
 import os, sys
+from wsgiref import types
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 import pprint
 from qdrant.client import get_client, get_reranker
@@ -9,12 +10,15 @@ import datetime
 import time
 # import retrieval pipeline as absolute path using importlib
 from qdrant.pipeline import retrieval_pipeline
-
+from google import genai
+from google.genai import types
+from dotenv import load_dotenv
+load_dotenv()
 # ------ TODO ------
 # modify query to ask what you want
 QUERY = "What does the New York times say is the best wireless headphones?"
 COLLECTION_NAME = "production_data"
-RESULT_COUNT = 10 # top n results to return
+RESULT_COUNT = 10 # top n results to returnc
 
 client = get_client()
 reranker_model = get_reranker()
@@ -44,3 +48,65 @@ with open(latency_path, "w") as f:
     f.write(f"Latency: {end - start:.2f} seconds\n")
     
 print(f"Results saved to: {out_path}")
+
+print("\n Retrieval Output:")
+context_texts = [res[0].payload["source_file"] for res in ranked_results]
+for i, text in enumerate(context_texts):
+    print(f"Context {i+1}:\n{text}\n")
+
+
+prompt = """
+You are a RAG system. Your task is to create a concise response in 3-5 lines based on the context provided. 
+The user query is given by the <user_query> tag and the relevant contexts are given with the <context> tag. 
+Only use the given context to ground your responses, do not use outside references or knowledge to answer the user query.
+"""
+
+user_query = """
+<user_query>
+{query}
+</user_query>   
+<context>
+{contexts}
+</context>
+""".format(query=QUERY, contexts="\n".join(context_texts))
+
+client = genai.Client()
+prompt = "Explain how AI works in a few words"
+response = client.models.generate_content(
+    model="gemini-2.5-flash",
+    config=types.GenerateContentConfig(
+        system_instruction=prompt),
+    contents=user_query,
+)
+print("\n LLM Response:")
+print(response.text)
+
+## LLM as a Judge 
+judge_prompt = """
+You are a strict judge. Your task is to evaluate the response based on the user query and the provided context in a likert scale from 1 to 5.
+1 - Poor: The response is irrelevant or incorrect based on the context.
+2 - Fair: The response has some relevance but lacks accuracy or completeness.
+3 - Good: The response is generally accurate but may miss some details from the context.
+4 - Very Good: The response is accurate and covers most of the important details from the context.
+5 - Excellent: The response is highly accurate, comprehensive, and directly addresses the user query using only the information from the context.
+
+"""
+judge_query = """<user_query>
+{query}
+</user_query>
+<context>
+{contexts}
+</context>
+<response>
+{response}
+</response>
+""".format(query=QUERY, contexts="\n".join(context_texts), response=response.text)
+
+judge_response = client.models.generate_content(
+    model="gemini-2.5-flash",
+    config=types.GenerateContentConfig(
+        system_instruction=judge_prompt),
+    contents=judge_query,
+)
+print("\nJudge's Evaluation:")
+print(judge_response.text)
